0a4bf43b3f84914efad1201ff027d71fc70e2a68
Viet Phan <viethoaiphan@gmail.com>
2020-01-21 08:09:28 -0600

    first commit

--- main.js
+++ main.js
@@ -22,9 +22,20 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
-
 // Add your functions below:
-
+function validateCred(numArr) {
+    let total = 0;
+    for (let i = numArr.length - 1; i >= 0; i--) {
+        let currValue = numArr[i]
+        if ((numArr.length - 1 - i) % 2 === 1) {
+            currValue *= 2;
+            if (currValue > 9) {
+                currValue -= 9;
+            }
+        }
+        total += currValue;
+    }
+}
 
 
 
